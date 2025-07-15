2aad5737c68cd2684338386d7a2c82668e322acd
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2025-07-15 12:19:50 -0400

    Some code cleanup
    compute_weights and compute_weights_full produce same output.

--- Solver/compute_weights.m
+++ Solver/compute_weights.m
@@ -1,46 +1,39 @@
-function [w_x, w_y, lbd3i] = compute_weights(C_xx,C_yy,C_xy, D_xy,f, gamma)
-%@Cxx covariance matrix of data at time X
-%@Cyy covariance matrix of data at time Y
-%@Cxy covariance matrix between data at time X and y
-%@Dxy covariance matrix between data at time X and Y, where the Data has
-%been shuffled
+function [w_x, w_y, lambda3] = compute_weights(C_xx, C_yy,C_xy, D_xy, f, gamma)
 
-% computes the weights w_x and w_y for Dimension f such that w_x*Data_x is maximally
-% correlated with Data_y * w_y, while keeping the correlation between
-% w_x*Data_Shuffled and Data_y*w_y at zero.
-if nargin>5
-    I = eye(size(C_xx,1));
-    C_xx = (1-gamma)* C_xx + gamma*I*trace(C_xx);
-    I = eye(size(C_yy,1));
-    C_yy = (1-gamma)* C_yy + gamma*I*trace(C_yy);
-end
-if nargin < 5
-    fun = @(l) foo2(C_xx,C_yy,C_xy, D_xy,l, 1);
-else
-    fun = @(l) foo2(C_xx,C_yy,C_xy, D_xy,l, f);
-end
-lbd3i = fminsearch(fun,0);
-
-M = inv(C_xx)*(C_xy+lbd3i*D_xy)*inv(C_yy)* ((C_xy+lbd3i*D_xy)');
-[W,D] = eigs(M,f);
-w_x = W(:,f);
-w_x = w_x./sqrt(w_x'*C_xx*w_x);
-lbd = sqrt(D(f,f));
-w_y = -inv(C_yy)*(C_xy+lbd3i*D_xy)'/lbd *w_x;
-if w_x'*C_xy*w_y < 0
-    w_y = -w_y; % The sign of "lbd" is not constrained. Make sure we maximize correlation
-end
+    if nargin == 5       %   Apply regularization on covariance matrices.
+        I = eye(size(C_xx,1));
+        C_xx = (1-gamma)* C_xx + gamma*I*trace(C_xx);
+        I = eye(size(C_yy,1));
+        C_yy = (1-gamma)* C_yy + gamma*I*trace(C_yy);
+        fun = @(l) foo2(C_xx,C_yy,C_xy, D_xy,l, f);
+    
+    elseif nargin == 4   %   Find "f" solutions.
+        fun = @(l) foo2(C_xx,C_yy,C_xy, D_xy,l, f);
+    else
+        fun = @(l) foo2(C_xx,C_yy,C_xy, D_xy,l, 1);
+    end
+    
+    lambda3 = fminsearch(fun,0);
+    
+    M = inv(C_xx)*(C_xy+lambda3*D_xy)*inv(C_yy)* ((C_xy+lambda3*D_xy)');
+    [W,D] = eigs(M,f);
+    w_x = W(:,f);
+    w_x = w_x./sqrt(w_x'*C_xx*w_x);
+    lbd = sqrt(D(f,f));
+    w_y = -inv(C_yy)*(C_xy+lambda3*D_xy)'/lbd *w_x;
+    if w_x'*C_xy*w_y < 0
+        w_y = -w_y; % The sign of "lbd" is not constrained. Make sure we maximize correlation
+    end
 
 end
-function [tst] = foo2(C_xx,C_yy,C_xy, D_xy,lbd3, f)
 
-% calculate the f largest eigenvalues only!
-M = inv(C_xx)*(C_xy+lbd3*D_xy)*inv(C_yy)* ((C_xy+lbd3*D_xy)');
-[W,D] = eigs(M,f);
-w_x = W(:,f);
-w_x = w_x./sqrt(w_x'*C_xx*w_x);
-w_y = inv(C_yy)*(C_xy+lbd3*D_xy)'*w_x;
-tst = abs(w_x'*D_xy*w_y);
-
+function [tst] = foo2(C_xx, C_yy, C_xy, D_xy, lbd3, f)
+    % calculate the f largest eigenvalues only!
+    M = inv(C_xx)*(C_xy+lbd3*D_xy)*inv(C_yy)* ((C_xy+lbd3*D_xy)');
+    [W,D] = eigs(M,f);
+    w_x = W(:,f);
+    w_x = w_x./sqrt(w_x'*C_xx*w_x);
+    w_y = inv(C_yy)*(C_xy+lbd3*D_xy)'*w_x;
+    tst = abs(w_x'*D_xy*w_y);
 end
 
