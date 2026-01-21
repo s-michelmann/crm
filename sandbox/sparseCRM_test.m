41590f030b484d1f3ee5921defd99078e4fb08a6
Manuel Schottdorf <manuel.schottdorf@gmail.com>
2026-01-21 15:17:34 -0500

    added normalization, plot details, more noise
    Seems alright. Back to Seb.

--- sandbox/sparseCRM_test.m
+++ sandbox/sparseCRM_test.m
@@ -1,23 +1,23 @@
 %% Test Script for Sparse CRM
 close all; clear all;
 
-n_samples = 10000;
+n_samples = 500;
 p = 22; % dimensions for x
 q = 40; % dimensions for y
 
 % True weights
-w_x_true = zeros(p, 1); w_x_true(1:5) = 1; % Only first 5 features relevant
-w_y_true = zeros(q, 1); w_y_true(1:5) = 1; 
+w_x_true = zeros(p, 1); w_x_true(randi(p,1,5)) = 1; % Pick a few random important relevant
+w_y_true = zeros(q, 1); w_y_true(randi(q,1,5)) = 1; 
 
 % Confound that we want to ignore
-d_x_true = zeros(p, 1); d_x_true(10:15) = 1; % Overlaps with different features randi(q,1,5)
-d_y_true = zeros(q, 1); d_y_true(10:15) = 1;
+d_x_true = zeros(p, 1); d_x_true(randi(p,1,5)) = 1; % Other overlapping features that we want to ignore 
+d_y_true = zeros(q, 1); d_y_true(randi(p,1,5)) = 1;
 
 % Generate Data
 S = randn(n_samples, 1);
 C = randn(n_samples, 1);
-X = S * w_x_true' + 2 * C * d_x_true' + 0.5 * randn(n_samples, p); % Data matrices: Signal + Confound + Noise
-Y = S * w_y_true' + 2 * C * d_y_true' + 0.5 * randn(n_samples, q);
+X = S * w_x_true' +  C * d_x_true' + randn(n_samples, p); % Data matrices: Signal + Confound + Noise
+Y = S * w_y_true' +  C * d_y_true' + randn(n_samples, q);
 
 % Covarince matrices like in normal CRM
 C_xx = (X' * X) / n_samples;
@@ -39,15 +39,27 @@ params.gamma = 0;    % Seb's Ridge term. For good measure
 
 figure(1),clf;
 
-subplot(1,2,1);
+subplot(2,2,1);
+imagesc(X');
+xlabel("Samples")
+title("X")
+
+subplot(2,2,2);
+imagesc(Y');
+xlabel("Samples")
+title("Y")
+
+subplot(2,2,3);
 stem(w_x, 'filled'); hold on;
+w_x_true = w_x_true ./ sqrt(w_x_true' * C_xx * w_x_true);
 stem(w_x_true, 'r--', 'LineWidth', 1.5);
 title('Weights for X');
 legend('Estimated w_x', 'True Signal Support');
 xlabel('Feature Index');
 
-subplot(1,2,2);
+subplot(2,2,4);
 stem(w_y, 'filled'); hold on;
+w_y_true = w_y_true ./ sqrt(w_y_true' * C_yy * w_y_true);
 stem(w_y_true, 'r--', 'LineWidth', 1.5);
 title('Weights for Y');
 legend('Estimated w_y', 'True Signal Support');
